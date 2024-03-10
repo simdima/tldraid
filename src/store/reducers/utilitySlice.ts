3a9c627001028bf1467b9db99796ba84917d4e33
Dima Sim <89066354+simdima@users.noreply.github.com>
2024-03-10 22:27:34 +0200

    Add delete functionality for bot answers

--- src/store/reducers/utilitySlice.ts
+++ src/store/reducers/utilitySlice.ts
@@ -1,12 +1,17 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 import { RootState } from '..';
 
-interface BotAnswers {
-  [utilityName: string]: string[];
+interface BotAnswer {
+  id: string;
+  content: string;
+}
+
+interface UtilityBotAnswers {
+  [utilityName: string]: BotAnswer[];
 }
 interface UtilityState {
   name: string;
-  botAnswers: BotAnswers;
+  botAnswers: UtilityBotAnswers;
 }
 
 const initialState: UtilityState = {
@@ -18,22 +23,27 @@ const utilitySlice = createSlice({
   name: 'utility',
   initialState,
   reducers: {
-    changeUtility: (state, { payload }: PayloadAction<string>) => {
-      state.name = payload;
+    changeUtility: (state, { payload: selectedUtility }: PayloadAction<string>) => {
+      state.name = selectedUtility;
     },
-    addBotAnswer: (state, { payload }: PayloadAction<string>) => {
+    addBotAnswer: (state, { payload: newBotAnswer }: PayloadAction<BotAnswer>) => {
       if (state.name) {
         if (!state.botAnswers[state.name]) {
           state.botAnswers[state.name] = [];
         }
 
-        state.botAnswers[state.name].push(JSON.stringify(payload));
+        state.botAnswers[state.name].push(newBotAnswer);
       }
+    },
+    deleteBotAnswer: (state, { payload: botAnswerId }: PayloadAction<string>) => {
+      state.botAnswers[state.name] = state.botAnswers[state.name].filter(
+        ({ id }) => id !== botAnswerId
+      );
     },
   },
 });
 
-export const { changeUtility, addBotAnswer } = utilitySlice.actions;
+export const { changeUtility, addBotAnswer, deleteBotAnswer } = utilitySlice.actions;
 
 export const selectUtilityName = (state: RootState) => state.utility.name;
 export const selectUtilityBotAnswers = (state: RootState) => state.utility.botAnswers;
