c983cf8079670280e8933e4aded39a8478629bbb
Dima Sim <89066354+simdima@users.noreply.github.com>
2024-06-05 20:58:07 +0300

    Remove animations and clean up tailwind config

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -8,7 +8,7 @@ const Introduction = (): JSX.Element | null => {
 
   return !utility ? (
     <Blockquote className="relative mx-auto w-11/12 text-center not-italic sm:w-96">
-      <span className="animate-fade-in-slowest opacity-0">Get summaries of </span>
+      <span className="animate-fade-in-slower opacity-0">Get summaries of </span>
       <a
         className="animate-fade-in whitespace-pre text-cyan-normal opacity-0"
         href="https://en.wikipedia.org/wiki/TLDR_Pages"
@@ -16,11 +16,11 @@ const Introduction = (): JSX.Element | null => {
       >
         tldr pages
       </a>
-      <span className="animate-fade-in-slowest opacity-0"> and supercharge your learning </span>
+      <span className="animate-fade-in-slower opacity-0"> and supercharge your learning </span>
       <span className="animate-fade-in whitespace-pre text-cyan-normal opacity-0">
         with AI-powered bot
       </span>
-      <span className="animate-fade-in-slowest opacity-0">
+      <span className="animate-fade-in-slower opacity-0">
         {' '}
         to help you find your way around any utility
       </span>
